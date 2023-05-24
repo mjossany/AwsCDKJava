177792cf527573520d12757b1496c12f0c2efb9e
Jossany Moura <mjossany@gmail.com>
2023-05-24 14:58:44 -0300

    Start project

---
+++ src/main/java/br/com/jossadev/aws_cdk_java/AwsCdkJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jossadev.aws_cdk_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCdkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCdkJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jossadev/aws_cdk_java/AwsCdkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jossadev.aws_cdk_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCdkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
